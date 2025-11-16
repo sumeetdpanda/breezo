00f9a394974138a281bdf416febe1a98cdc63931
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2025-11-16 05:48:49 +0530

    Auth Controller Implemented

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
